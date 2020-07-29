85227466efa257678135c9671bc82c8fec9da16a
eternal golden bread <codylewisbrock@gmail.com>
2020-07-29 14:28:32 -0700

    continues styling.  Adds $ before amounts, resizes form, adds in spot for description

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './Form.css';
+
 class Form extends Component {
 
   constructor(props) {
@@ -26,20 +28,54 @@ class Form extends Component {
       <ul className="list-group list-group-flush">
         <li className="list-group-item">
           <form>
+
             <div className="form-group row">
-              <label htmlFor={this.props.formLabel} className="col-sm-2 col-form-label">
-                {this.capitalizeFirstLetter(this.props.formLabel)}
-              </label>
-              <div className="col-sm-10">
-                <input 
-                  type="number" 
-                  name={this.props.formLabel}
-                  id={this.props.formLabel}
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                  className="form-control"
-                />
+
+              <div className="col-sm-9">
+                <label htmlFor={this.props.formLabel} className="col-form-label label-title">
+                  {this.props.formLabel.toUpperCase()}
+                </label>
+                <div className="label-description">
+                  Description here
+                </div>
               </div>
+
+
+
+              {/* <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">$</span>
+                  <span class="input-group-text">0.00</span>
+                </div>
+                <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+              </div>
+
+              <div class="input-group">
+                <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                <div class="input-group-append">
+                  <span class="input-group-text">$</span>
+                  <span class="input-group-text">0.00</span>
+                </div>
+              </div> */}
+
+
+
+              <div className="col-sm-3">
+                <div className="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text" id="inputGroup-sizing-sm">$</div>
+                  </div>
+                  <input 
+                    type="number" 
+                    name={this.props.formLabel}
+                    id={this.props.formLabel}
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    className="form-control"
+                  />
+                </div>
+              </div>
+
             </div>
           </form>
         </li>
