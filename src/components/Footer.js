7ce0de26677f2a0610a9e3ba19404693b22e69e1
eternal golden bread <codylewisbrock@gmail.com>
2020-09-26 23:25:51 -0700

    changing the footer - simpler, smaller.  Adding contact information.  Should style it horizontally next

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,90 +3,15 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return(
-      // Footer
-      // navbar navbar-expand navbar-dark bg-dark
-      <footer class="footer-nav  navbar-dark bg-dark font-small blue pt-4">
 
-        {/* Footer Links */}
-        <div class="container-fluid text-center text-md-left">
-
-          {/* Grid row */}
-          <div class="row">
-
-            {/* Grid column */}
-            <div class="col-md-6 mt-md-0 mt-3">
-
-              {/* Content */}
-              <h5 class="text-uppercase">Footer Content</h5>
-              <p>Here you can use rows and columns to organize your footer content.</p>
-
-            </div>
-            {/* Grid column */}
-
-            <hr class="clearfix w-100 d-md-none pb-3" />
-
-            {/* Grid column */}
-            <div class="col-md-3 mb-md-0 mb-3">
-
-              {/* Links */}
-              <h5 class="text-uppercase">Links</h5>
-
-              <ul class="list-unstyled">
-                <li>
-                  <a href="#!">Link 1</a>
-                </li>
-                <li>
-                  <a href="#!">Link 2</a>
-                </li>
-                <li>
-                  <a href="#!">Link 3</a>
-                </li>
-                <li>
-                  <a href="#!">Link 4</a>
-                </li>
-              </ul>
-
-            </div>
-            {/* Grid column */}
-
-            {/* Grid column */}
-            <div class="col-md-3 mb-md-0 mb-3">
-
-              {/* Links */}
-              <h5 class="text-uppercase">Links</h5>
-
-              <ul class="list-unstyled">
-                <li>
-                  <a href="#!">Link 1</a>
-                </li>
-                <li>
-                  <a href="#!">Link 2</a>
-                </li>
-                <li>
-                  <a href="#!">Link 3</a>
-                </li>
-                <li>
-                  <a href="#!">Link 4</a>
-                </li>
-              </ul>
-
-            </div>
-            {/* Grid column */}
-
-          </div>
-          {/* Grid row */}
-
+      <footer class="footer-nav navbar-dark bg-dark">
+        <div className="text-center">© 2020 Copyright ||
+          <div> Cody Brock</div>
+          <a href="https://codybrock.dev/">Portfolio</a>
+          <a href="https://github.com/cody-brock">Github</a>
         </div>
-        {/* Footer Links */}
-
-        {/* Copyright */}
-        <div class="footer-copyright text-center py-3">© 2020 Copyright:
-          <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-        </div>
-        {/* Copyright */}
 
       </footer>
-      // Footer
 
     )
   }
