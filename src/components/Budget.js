317af81e355d566943886af091342c4ffa1c9324
eternal golden bread <codylewisbrock@gmail.com>
2020-09-29 14:30:29 -0700

    Need to step away, so leaving this in a messy but functional state.  Adds functionality to the Budget component to identify and handle unregistered users.  To do - clean up the code, remove repitition, style start now button on home

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import SweetAlert from 'react-bootstrap-sweetalert';
+import Swal from 'sweetalert2';
+// import { validateUsername, validateEmail, validatePassword } from "./shared/helpers";
+
 
 // import { v4 as uuidv4 } from "uuid";
 
@@ -11,6 +15,7 @@ import userService from "../services/user.service.js";
 // Objects containing the default income and expense data, in the event a new user
 import { incomeData } from "./shared/newUserSeed";
 import { expenseData } from "./shared/newUserSeed";
+import HelloWorld from "./ModalSignUp.js";
 
 
 class Budget extends Component {
@@ -36,9 +41,10 @@ class Budget extends Component {
       incomeTotal: 0,
       expenseTotal: 0,
 
-      username: "",
-      email: "",
-      password: "",
+      username: undefined,
+      email: undefined,
+      password: undefined,
+      alert: false,
     }
     this.updateValue            = this.updateValue.bind(this);
     this.updateCategoryTotal    = this.updateCategoryTotal.bind(this);
@@ -51,6 +57,7 @@ class Budget extends Component {
     this.handleSave             = this.handleSave.bind(this);
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
 
   }
 
@@ -141,9 +148,242 @@ class Budget extends Component {
     this.saveNewField(obj, "expensesData", category);
   }
 
+
+  // ***********************************************
+  // TO REFACTOR - repetition from SignUp component.
+
+  handleSignUp(e) {
+    // e.preventDefault();
+
+    if (!this.state.username || !this.state.email || !this.state.password) {
+      this.setState({alert: false})
+      Swal.fire({
+        icon: 'warning',
+        title: 'Oops!',
+        text: 'Please fill out all forms.',
+      }).then(() => this.toggleSignUp());
+    // } 
+    // else if (validateEmail(this.state.email) === false) {
+    //   this.setState({alert: false})
+    //   Swal.fire({
+    //     icon: 'warning',
+    //     title: 'Oops!',
+    //     text: 'Please enter a valid email address.',
+    //   }).then(() => this.toggleSignUp());
+    } else if (this.state.username.length < 3 || this.state.username.length > 20) {
+      this.setState({alert: false})
+      Swal.fire({
+        icon: 'warning',
+        title: 'Oops!',
+        text: 'Username must be between 3 and 20 characters long.',
+      }).then(() => this.toggleSignUp());
+    } else if (this.state.password.length < 6 || this.state.password.length > 40) {
+      this.setState({alert: false})
+      Swal.fire({
+        icon: 'warning',
+        title: 'Oops!',
+        text: 'Password must be between 6 and 40 characters long.',
+      }).then(() => this.toggleSignUp());
+    } else {
+      this.setState({
+        message: "",
+        successful: false
+      });
+    
+      AuthService.signup(
+        this.state.username,
+        this.state.email,
+        this.state.password
+      )
+        .then( () => {
+          this.setState({
+            successful: true
+          });
+          Swal.fire({
+            icon: 'success',
+            title: 'Success!',
+            html: 'You are now registered!<br><br>Redirecting you to your dashboard...',
+            showConfirmButton: false,
+            timer: 1500
+          })
+            .then( () => {
+              AuthService.login(this.state.username, this.state.password)
+              // .then( () => {
+              //   userService.createNewUserItems()
+              // })
+                .then( () => {
+                  this.props.history.push("/dashboard");
+                  window.location.reload();
+                }).catch(error => {
+                  const resMessage =
+                    (error.response &&
+                      error.response.data &&
+                      error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                    
+                  this.setState({ loading: false });
+                  Swal.fire({
+                    icon: 'warning',
+                    title: 'Oops!',
+                    text: `${resMessage} Please try again.`, 
+                    footer: 'Or, if you have not yet signed up, please do so.'
+                  })
+                });
+        },
+        error => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          this.setState({ successful: false });
+
+          Swal.fire({
+            icon: 'warning',
+            title: 'Oops!',
+            text: `${resMessage} Please try again.`, 
+            footer: 'Or, if you have already signed up, please go to the Log In page.'
+          });
+        }
+      );
+    })
+  }
+}
+
+
   handleChange(evt) {
     this.setState({[evt.target.name]: evt.target.value});
   }
+
+  toggleSignUp() {
+    let btnStyle = {
+      padding: "7px 65px",
+      borderRadius: "20px",
+      backgroundColor: "rgba(13, 97, 72, 0.842)",
+      color: "rgb(255, 255, 255)"
+    }
+
+    console.log("this.state.username: ", this.state.username)
+    console.log("this.state.email: ", this.state.email)
+    console.log("this.state.password: ", this.state.password)
+
+    this.setState({username: undefined, email: undefined, password: undefined});
+
+
+    const getAlert = () => (
+
+    // this.setState({[incOrExp]: newState}, () => {
+    //   this.updateCategoryTotal(incOrExp, category);
+    // })
+
+
+    <SweetAlert
+      title="Oops!"
+      onConfirm={this.handleSignUp}
+      confirmBtnStyle={btnStyle}
+      onCancel={this.onCancel}
+      type="controlled"
+      // dependencies={[this.state.username, this.state.email, this.state.password]}
+      >
+
+        <form>
+          <div className="form-group">
+          It seems you have not yet signed up.  In order for us to save your budget, please register below.
+          <hr/>
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="card login-label">
+                  <label htmlFor="username">Username</label>
+                  <input 
+                    type="text" 
+                    name="username"
+                    value={this.state.username}
+                    onChange={(e) => this.setState({ username: e.target.value })}
+                    className="form-control login-form" 
+                    placeholder="Enter Username Here" 
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="card login-label">
+                  <label htmlFor="email">Email</label>
+                  <input 
+                    type="text" 
+                    name="email"
+                    value={this.state.email}
+                    onChange={(e) => this.setState({ email: e.target.value })}
+                    className="form-control login-form" 
+                    placeholder="youremail@gmail.com" 
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="card login-label">
+                  <label htmlFor="password">Password</label>
+                  <input 
+                    type="password" 
+                    name="password"
+                    value={this.state.password}
+                    onChange={(e) => this.setState({ password: e.target.value })}
+                    className="form-control login-form" 
+                    placeholder="Enter Password Here"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="row">
+              <div className="col-sm-12">
+                <button
+                  className="btn btn-signup"
+                  disabled={this.state.loading}
+                  onClick={this.handleSignUp}
+                  >
+                  {this.state.loading && (
+                    <span className="spinner-border spinner-border-sm"></span>
+                    )}
+                  <span>Sign Up</span>
+                </button>
+              </div>
+            </div> */}
+          </div>
+        </form>
+
+
+
+        
+      </SweetAlert>
+
+      
+    );
+
+
+
+
+
+    this.setState({
+      alert: getAlert()
+    });
+  }
+  // TO REFACTOR - repetition from SignUp component.
+  // ***********************************************
+
+  hideAlert() {
+    console.log('Hiding alert...');
+    this.setState({
+      alert: null
+    });
+  }
+
 
 
   handleSave(evt) {    
@@ -152,7 +392,11 @@ class Budget extends Component {
     if (this.state.unregisteredUser) {
       // handle unregistered user flow
       console.log("handleSave has been called with an unregistered user");
+      this.toggleSignUp();
+      // this
 
+
+      // this.setState({ unregisteredUser: false });
 
 
 
@@ -295,6 +539,8 @@ class Budget extends Component {
             // TODO: 9/22 - add a loading image
             <div>Loading</div>
         }
+
+      {this.state.alert}
 
       </div>
     );
